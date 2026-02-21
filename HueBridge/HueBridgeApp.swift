61fb062d903f3a6bbfb37c9843461cba7668cc8f
1so-H3an <lhc2005523@163.com>
2026-02-21 16:12:04 +0800

    Add files via upload

---
+++ HueBridge/HueBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HueBridgeApp.swift
+//  HueBridge
+//
+//  Created by 梁航川 on 2026/2/21.
+//
+
+import SwiftUI
+
+@main
+struct HueBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
